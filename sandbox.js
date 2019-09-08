c8bb55bb5388a94b8bba19f9402e59efe08b5c94
Tomasz Swiatek <tomasswiatek@gmail.com>
2019-09-08 11:21:34 +0200

    got data and ve shown in the console

---
+++ sandbox.js
@@ -0,0 +1,30 @@
+const list = document.querySelector("ul");
+const addTodo = todo => {
+  let html = `
+
+    <li class="">
+    <div>${todo.title}</div>
+    <div>${todo.created_at}</div>
+    <div>${todo.author}</div>
+    </li>
+
+    `;
+  console.log(html);
+};
+
+//get todos from firestore:
+db.collection("todos")
+  .get()
+  .then(snapshot => {
+    //this is the time when we ve got data:
+    //snapshot it is an image how our collection looks at that minute.
+    // console.log("data fetched", snapshot.docs[0].data());
+    //reference snapshot.docs is an array and we can cycle through and use for each item built in method call data() to get the data:
+    snapshot.docs.forEach(doc => {
+      //   console.log(doc.data());
+      addTodo(doc.data());
+    });
+  })
+  .catch(err => {
+    console.log(err);
+  });
