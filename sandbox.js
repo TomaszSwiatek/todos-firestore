db7b0df1c4f4e279af02f82d3e1bbe67e9dae540
Tomasz Swiatek <tomasswiatek@gmail.com>
2019-09-19 20:49:55 +0200

    added real-time data listener approach

--- sandbox.js
+++ sandbox.js
@@ -14,23 +14,48 @@ const addTodo = (todo, id) => {
     `;
   list.innerHTML += html;
 };
-
+//WE GOT RID OF THIS, BECOUSE WE DEPLOY REAL TIME LISTENER APPROACH UNDERNEATH.
 //get todos from firestore:
-db.collection("todos")
-  .get()
-  .then(snapshot => {
-    //this is the time when we ve got data:
-    //snapshot it is an image how our collection looks at that minute.
-    // console.log("data fetched", snapshot.docs[0].data());
-    //reference snapshot.docs is an array and we can cycle through and use for each item built in method call data() to get the data:
-    snapshot.docs.forEach(doc => {
-      // console.log(doc.id);
-      addTodo(doc.data(), doc.id);
-    });
-  })
-  .catch(err => {
-    console.log(err);
+// db.collection("todos")
+//   .get()
+//   .then(snapshot => {
+//     //this is the time when we ve got data:
+//     //snapshot it is an image how our collection looks at that minute.
+//     // console.log("data fetched", snapshot.docs[0].data());
+//     //reference snapshot.docs is an array and we can cycle through and use for each item built in method call data() to get the data:
+//     snapshot.docs.forEach(doc => {
+//       // console.log(doc.id);
+//       addTodo(doc.data(), doc.id);
+//     });
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
+
+//get real time data listener (listen for every data change):
+//firstly we re creating func expression to delete todos:
+const deleteTodo = id => {
+  //we already deleting todos from firebase at event listener. Here we only need to delete todo from template.
+  const todos = document.querySelectorAll("li");
+  todos.forEach(todo => {
+    if (todo.getAttribute("data-id") === id) {
+      todo.remove();
+    }
   });
+};
+
+db.collection("todos").onSnapshot(snapshot => {
+  snapshot.docChanges().forEach(change => {
+    const doc = change.doc;
+    //get todos and show on website:
+    if (change.type === "added") {
+      addTodo(doc.data(), doc.id);
+      //here we remove chosen todo from DOM, after todo was deleted from firebase:
+    } else if (change.type === "removed") {
+      deleteTodo(doc.id);
+    }
+  });
+});
 
 //SAVING TODOS
 //firstly create submit event on form to send todos:
